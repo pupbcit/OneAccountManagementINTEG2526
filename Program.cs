fd05705165cf030db1737c770fec315507a8e985
indaleenq <inquinsayas@gmail.com>
2026-02-14 16:39:15 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OneAccountManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ACCOUNT MANAGEMENT SYSTEM");
+
+            string username = "superuser";
+            string password = "superuser123!";
+
+            Console.Write("Enter username: ");
+            string usernameInput = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string passwordInput = Console.ReadLine();
+
+            if (usernameInput == username && passwordInput == password)
+            {
+                Console.WriteLine("Login successful.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials.");
+            }
+        }
+    }
+}
